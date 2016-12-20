5a520b4f523936d391088815316213cea74d038c
Tom Conway <tconway@au1.ibm.com>
2016-12-20 15:45:29 +1100

    A bit of reorganization for packaging.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+setup(name='zotmer',
+        version='0.1',
+        description='A python k-mer application workbench',
+        url='http://github.com/drtconway/pykmer',
+        author='Tom Conway',
+        author_email='drtomc@gmail.com',
+        license='Apache2',
+        keywords='bioinformatics genomics pathogenomics',
+        packages=find_packages(),
+        install_requires=['docopt', 'pykmer'],
+        entry_points = {
+            'console_scripts' : ['zot=zotmer.cli:main']
+        }
+        zip_safe=False)
