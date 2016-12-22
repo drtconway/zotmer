ddc513d620c463572299f5388b4c18a90dd31e62
Tom Conway <tconway@au1.ibm.com>
2016-12-23 10:57:22 +1100

    Generalize merge to work on different input/output types.

--- zotmer/commands/merge.py
+++ zotmer/commands/merge.py
@@ -1,51 +1,64 @@
 """
 Usage:
-    zot merge <output> <input>...
+    zot merge [(-s|-f)] <output> <input>...
+
+Options:
+    -s          force output to be a k-mer set
+    -f          force output to be a k-mer frequency set
 """
 
-from pykmer.basics import render
+from pykmer.adaptors import k2kf, kf2k
+from pykmer.container import probe
+import pykmer.kset as kset
 import pykmer.kfset as kfset
 
 import docopt
+import sys
 
-def merge1(k, xs, ys):
+def merge(k, xs, ys, access):
     moreXs = True
     moreYs = True
 
     try:
         x = xs.next()
+        xk = access(x)
     except StopIteration:
         moreXs = False
     try:
         y = ys.next()
+        yk = access(y)
     except StopIteration:
         moreYs = False
 
     while moreXs and moreYs:
-        if x < y:
+        if xk < yk:
             yield x
             try:
                 x = xs.next()
+                xk = access(x)
             except StopIteration:
                 moreXs = False
             continue
 
-        if x > y:
+        if xk > yk:
             yield y
             try:
                 y = ys.next()
+                yk = access(y)
             except StopIteration:
                 moreYs = False
             continue
 
-        assert x == y
+        assert xk == yk
         yield x
         try:
             x = xs.next()
+            xk = access(x)
         except StopIteration:
             moreXs = False
         try:
             y = ys.next()
+            yk = access(y)
         except StopIteration:
             moreYs = False
 
@@ -63,78 +76,67 @@ def merge1(k, xs, ys):
         except StopIteration:
             moreYs = False
 
-def merge2(k, xs, ys):
-    moreXs = True
-    moreYs = True
-
-    try:
-        (x,xf) = xs.next()
-    except StopIteration:
-        moreXs = False
-    try:
-        (y,yf) = ys.next()
-    except StopIteration:
-        moreYs = False
-
-    while moreXs and moreYs:
-        if x < y:
-            yield (x, xf)
-            try:
-                (x,xf) = xs.next()
-            except StopIteration:
-                moreXs = False
-            continue
-
-        if x > y:
-            yield (y, yf)
-            try:
-                (y,yf) = ys.next()
-            except StopIteration:
-                moreYs = False
-            continue
-
-        assert x == y
-        yield (x, xf + yf)
-        try:
-            (x,xf) = xs.next()
-        except StopIteration:
-            moreXs = False
-        try:
-            (y,yf) = ys.next()
-        except StopIteration:
-            moreYs = False
-
-    while moreXs:
-        yield (x, xf)
-        try:
-            (x,xf) = xs.next()
-        except StopIteration:
-            moreXs = False
-
-    while moreYs:
-        yield (y, yf)
-        try:
-            (y,yf) = ys.next()
-        except StopIteration:
-            moreYs = False
-
 def main(argv):
     opts = docopt.docopt(__doc__, argv)
 
     K = None
-    zs = None
-    out = opts['<output>']
+    types = []
+    itrs = []
+    ksets = 0
+    kfsets = 0
     for inp in opts['<input>']:
-        (m, xs) = kfset.read(inp)
+        (m, _) = probe(inp)
+
         K0 = m['K']
-        if K is None :
+        if K is None:
             K = K0
-            zs = xs
-        else:
-            if K != K0:
+        elif K0 != K:
                 raise MismatchedK(K, K0)
-            zs = merge2(K, zs, xs)
-    if K is not None:
+
+        if m['type'] == 'k-mer set':
+            (_, xs) = kfset.read(inp)
+            types.append(False)
+            itrs.append(xs)
+            ksets += 1
+        else:
+            (_, xs) = kfset.read(inp)
+            types.append(True)
+            itrs.append(xs)
+            kfsets += 1
+
+    outFmt = None
+    if opts['-s'] is not None and opts['-s']:
+        outFmt = False
+    elif opts['-f'] is not None and opts['-f']:
+        outFmt = True
+    elif ksets > 0:
+        outFmt = False
+    else:
+        outFmt = True
+
+    out = opts['<output>']
+
+    if outFmt == False:
+        zs = None
+        for i in xrange(len(types)):
+            xs = itrs[i]
+            if types[i] != outFmt:
+                xs = kf2k(xs)
+            if zs is None:
+                zs = xs
+            else:
+                zs = merge(K, zs, xs, lambda x: x)
+        kset.write(K, zs, out)
+    else:
+        zs = None
+        for i in xrange(len(types)):
+            xs = itrs[i]
+            if types[i] != outFmt:
+                xs = k2kf(xs)
+            if zs is None:
+                zs = xs
+            else:
+                zs = merge(K, zs, xs, lambda x: x[0])
         kfset.write(K, zs, out)
 
 if __name__ == '__main__':
