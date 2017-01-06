56734501971188bb6b4cf7caf519f24e10dca23a
Tom Conway <tconway@au1.ibm.com>
2017-01-06 15:49:13 +1100

    Enhance spoligotyping so it can use other schemes.

--- zotmer/commands/spoligo.py
+++ zotmer/commands/spoligo.py
@@ -1,65 +1,133 @@
 """
 Usage:
-    zot spoligo <input>...
+    zot spoligo [-lp PROBES] <input>...
 
-Perform MTB spoligotyping on sets of k-mers.
+Perform spoligotyping on sets of k-mers. By default, use the MTB
+spoligotyping probes. If the -p option is used, then the argument
+is a file name of a text file continaining an optional name and a
+probe on each line. Lines starting with '#' are ignored.
+
+Options:
+    -l          use long output format with one line per probe
+    -p PROBES   the name of a file containing probe sequences
 """
 
 from pykmer.adaptors import kf2k
 from pykmer.basics import kmer, render
 from pykmer.misc import uniq
 from pykmer.sparse import sparse
-from pykmer.container import probe
+import pykmer.container as container
 import pykmer.kfset as kfset
 import pykmer.kset as kset
 
+import array
 import docopt
 import sys
 
+def neigh(K, x, d):
+    if d == 0:
+        return []
+    xs = []
+    for i in xrange(K):
+        for j in xrange(3):
+            xs.append(x ^ ((j + 1) << (2*i)))
+    xs.sort()
+
+    if d == 1:
+        return xs
+
+    zs = []
+    for y in xs:
+        zs += neigh(K, y, d - 1)
+    zs.sort()
+    uniq(zs)
+    diff(zs, [x])
+    diff(zs, xs)
+    return zs
+
+def probe(s):
+    return (len(s), kmer(s))
+
+def findApprox(J, x, K, xs, D):
+    assert J <= K
+
+    s = 2*(K - J)
+    y0 = x << s
+    y1 = (x+1) << s
+    (r0, r1) = xs.rank2(y0, y1)
+    if r1 - r0 > 0:
+        return True
+
+    for d in xrange(1, D+1):
+        for y in neigh(J, x, d):
+            y0 = y << s
+            y1 = (y+1) << s
+            (r0, r1) = xs.rank2(y0, y1)
+            if r1 - r0 > 0:
+                return True
+    return False
+
+def findProbe(p, K, xs):
+    Kp = p[0]
+    x = p[1]
+    if Kp <= K:
+        return findApprox(Kp, x, K, xs, 2)
+
+    # Kp > K.
+    M = (1 << (2*K)) - 1
+    y0 = x
+    ys = []
+    for i in xrange(1 + Kp - K):
+        y = y0 & M
+        if not findApprox(K, y, K, xs, 2):
+            return False
+        y0 >>= 2
+    return True
+
 probesMTB = [
-    kmer('ATAGAGGGTCGCCGGTTCTGGATCA'),
-    kmer('CCTCATAATTGGGCGACAGCTTTTG'),
-    kmer('CCGTGCTTCCAGTGATCGCCTTCTA'),
-    kmer('ACGTCATACGCCGACCAATCATCAG'),
-    kmer('TTTTCTGACCACTTGTGCGGGATTA'),
-    kmer('CGTCGTCATTTCCGGCTTCAATTTC'),
-    kmer('GAGGAGAGCGAGTACTCGGGGCTGC'),
-    kmer('CGTGAAACCGCCCCCAGCCTCGCCG'),
-    kmer('ACTCGGAATCCCATGTGCTGACAGC'),
-    kmer('TCGACACCCGCTCTAGTTGACTTCC'),
-    kmer('GTGAGCAACGGCGGCGGCAACCTGG'),
-    kmer('ATATCTGCTGCCCGCCCGGGGAGAT'),
-    kmer('GACCATCATTGCCATTCCCTCTCCC'),
-    kmer('GGTGTGATGCGGATGGTCGGCTCGG'),
-    kmer('CTTGAATAACGCGCAGTGAATTTCG'),
-    kmer('CGAGTTCCCGTCAGCGTCGTAAATC'),
-    kmer('GCGCCGGCCCGCGCGGATGACTCCG'),
-    kmer('CATGGACCCGGGCGAGCTGCAGATG'),
-    kmer('TAACTGGCTTGGCGCTGATCCTGGT'),
-    kmer('TTGACCTCGCCAGGAGAGAAGATCA'),
-    kmer('TCGATGTCGATGTCCCAATCGTCGA'),
-    kmer('ACCGCAGACGGCACGATTGAGACAA'),
-    kmer('AGCATCGCTGATGCGGTCCAGCTCG'),
-    kmer('CCGCCTGCTGGGTGAGACGTGCTCG'),
-    kmer('GATCAGCGACCACCGCACCCTGTCA'),
-    kmer('CTTCAGCACCACCATCATCCGGCGC'),
-    kmer('GGATTCGTGATCTCTTCCCGCGGAT'),
-    kmer('TGCCCCGGCGTTTAGCGATCACAAC'),
-    kmer('AAATACAGGCTCCACGACACGACCA'),
-    kmer('GGTTGCCCCGCGCCCTTTTCCAGCC'),
-    kmer('TCAGACAGGTTCGCGTCGATCAAGT'),
-    kmer('GACCAAATAGGTATCGGCGTGTTCA'),
-    kmer('GACATGACGGCGGTGCCGCACTTGA'),
-    kmer('AAGTCACCTCGCCCACACCGTCGAA'),
-    kmer('TCCGTACGCTCGAAACGCTTCCAAC'),
-    kmer('CGAAATCCAGCACCACATCCGCAGC'),
-    kmer('CGCGAACTCGTCCACAGTCCCCCTT'),
-    kmer('CGTGGATGGCGGATGCGTTGTGCGC'),
-    kmer('GACGATGGCCAGTAAATCGGCGTGG'),
-    kmer('CGCCATCTGTGCCTCATACAGGTCC'),
-    kmer('GGAGCTTTCCGGCTTCTATCAGGTA'),
-    kmer('ATGGTGGGACATGGACGAGCGCGAC'),
-    kmer('CGCAGAATCGCACCGGGTGCGGGAG')
+    probe('ATAGAGGGTCGCCGGTTCTGGATCA'),
+    probe('CCTCATAATTGGGCGACAGCTTTTG'),
+    probe('CCGTGCTTCCAGTGATCGCCTTCTA'),
+    probe('ACGTCATACGCCGACCAATCATCAG'),
+    probe('TTTTCTGACCACTTGTGCGGGATTA'),
+    probe('CGTCGTCATTTCCGGCTTCAATTTC'),
+    probe('GAGGAGAGCGAGTACTCGGGGCTGC'),
+    probe('CGTGAAACCGCCCCCAGCCTCGCCG'),
+    probe('ACTCGGAATCCCATGTGCTGACAGC'),
+    probe('TCGACACCCGCTCTAGTTGACTTCC'),
+    probe('GTGAGCAACGGCGGCGGCAACCTGG'),
+    probe('ATATCTGCTGCCCGCCCGGGGAGAT'),
+    probe('GACCATCATTGCCATTCCCTCTCCC'),
+    probe('GGTGTGATGCGGATGGTCGGCTCGG'),
+    probe('CTTGAATAACGCGCAGTGAATTTCG'),
+    probe('CGAGTTCCCGTCAGCGTCGTAAATC'),
+    probe('GCGCCGGCCCGCGCGGATGACTCCG'),
+    probe('CATGGACCCGGGCGAGCTGCAGATG'),
+    probe('TAACTGGCTTGGCGCTGATCCTGGT'),
+    probe('TTGACCTCGCCAGGAGAGAAGATCA'),
+    probe('TCGATGTCGATGTCCCAATCGTCGA'),
+    probe('ACCGCAGACGGCACGATTGAGACAA'),
+    probe('AGCATCGCTGATGCGGTCCAGCTCG'),
+    probe('CCGCCTGCTGGGTGAGACGTGCTCG'),
+    probe('GATCAGCGACCACCGCACCCTGTCA'),
+    probe('CTTCAGCACCACCATCATCCGGCGC'),
+    probe('GGATTCGTGATCTCTTCCCGCGGAT'),
+    probe('TGCCCCGGCGTTTAGCGATCACAAC'),
+    probe('AAATACAGGCTCCACGACACGACCA'),
+    probe('GGTTGCCCCGCGCCCTTTTCCAGCC'),
+    probe('TCAGACAGGTTCGCGTCGATCAAGT'),
+    probe('GACCAAATAGGTATCGGCGTGTTCA'),
+    probe('GACATGACGGCGGTGCCGCACTTGA'),
+    probe('AAGTCACCTCGCCCACACCGTCGAA'),
+    probe('TCCGTACGCTCGAAACGCTTCCAAC'),
+    probe('CGAAATCCAGCACCACATCCGCAGC'),
+    probe('CGCGAACTCGTCCACAGTCCCCCTT'),
+    probe('CGTGGATGGCGGATGCGTTGTGCGC'),
+    probe('GACGATGGCCAGTAAATCGGCGTGG'),
+    probe('CGCCATCTGTGCCTCATACAGGTCC'),
+    probe('GGAGCTTTCCGGCTTCTATCAGGTA'),
+    probe('ATGGTGGGACATGGACGAGCGCGAC'),
+    probe('CGCAGAATCGCACCGGGTGCGGGAG')
 ]
 
 def diff(xs, ys):
@@ -90,32 +158,39 @@ def diff(xs, ys):
             k += 1
         del xs[k:]
 
-def neigh(K, x, d):
-    if d == 0:
-        return []
-    xs = []
-    for i in xrange(K):
-        for j in xrange(3):
-            xs.append(x ^ ((j + 1) << (2*i)))
-    xs.sort()
-
-    if d == 1:
-        return xs
-
-    zs = []
-    for y in xs:
-        zs += neigh(K, y, d - 1)
-    zs.sort()
-    uniq(zs)
-    diff(zs, [x])
-    diff(zs, xs)
-    return zs
-
 def main(argv):
     opts = docopt.docopt(__doc__, argv)
 
+    nms = [str(i+1) for i in xrange(len(probesMTB))]
+    probes = probesMTB
+
+    if opts['-p'] is not None:
+        nms = []
+        probes = []
+        bad = False
+        with open(opts['-p']) as f:
+            i = 0
+            ln = 0
+            for l in f:
+                ln += 1
+                if l[0] == '#':
+                    continue
+                i += 1
+                t = l.split()
+                if len(t) == 1:
+                    nms.append(str(i))
+                    probes.append(probe(t[0]))
+                elif len(t) == 2:
+                    nms.append(t[0])
+                    probes.append(probe(t[1]))
+                else:
+                    bad = True
+                    print >> sys.stderr, '%s line %d, badly formatted.' % (opts['-p'], i)
+        if bad:
+            sys.exit(1)
+
     for inp in opts['<input>']:
-        (m, _) = probe(inp)
+        (m, _) = container.probe(inp)
         if m['type'] == 'k-mer set':
             (m, xs) = kset.read(inp)
             K = m['K']
@@ -124,39 +199,19 @@ def main(argv):
             K = m['K']
             xs = kf2k(xs)
 
-        # all MTB probes are 25bp
-        J = 2*(K - 25)
-        if J < 0:
-            print >> sys.stderr, "K < 25. Cannot reliably spoligotype."
-            sys.exit(1)
-
-        idx = {}
-
-        for i in xrange(len(probesMTB)):
-            idx[probesMTB[i]] = i
-            for y in neigh(25, probesMTB[i], 1):
-                if y in idx:
-                    print >> sys.stderr, 'ambiguity between probes %d and %d' % (i, idx[y])
-                idx[y] = i
-            for y in neigh(25, probesMTB[i], 2):
-                if y in idx:
-                    print >> sys.stderr, 'ambiguity between probes %d and %d' % (i, idx[y])
-                idx[y] = i
-
-        counts = [0 for i in xrange(len(probesMTB))]
-
-        for x in xs:
-            y = x >> J
-            if y in idx:
-                counts[idx[y]] += 1
+        xs = sparse(2*K, array.array('L', xs))
 
         res = []
-        for i in xrange(len(counts)):
-            if counts[i] > 0:
+        for i in xrange(len(probes)):
+            if findProbe(probes[i], K, xs):
                 res.append('1')
             else:
                 res.append('0')
-        print inp + '\t' + ''.join(res)
+        if opts['-l']:
+            for i in xrange(len(nms)):
+                print '%s\t%s\t%s' % (inp, nms[i], res[i])
+        else:
+            print inp + '\t' + ''.join(res)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
