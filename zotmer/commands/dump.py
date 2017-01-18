b27634635e9c7cb8570b6dfda1d3cb0d24cadc2e
Tom Conway <tconway@au1.ibm.com>
2017-01-18 13:48:36 +1100

    Sync kmer dumping.

--- zotmer/commands/dump.py
+++ zotmer/commands/dump.py
@@ -1,11 +1,11 @@
 """
 Usage:
-    zot dump <input>...
+    zot dump <input>
 """
 
 from pykmer.basics import render
 from pykmer.container import container
-from pykmer.container.std import readKmers, readCounts, readKmersAndCounts
+from pykmer.container.std import readKmers, readKmersAndCounts
 
 import docopt
 import sys
@@ -13,21 +13,20 @@ import sys
 def main(argv):
     opts = docopt.docopt(__doc__, argv)
 
-    for inp in opts['<input>']:
-        with container(inp) as z:
-            for nm in z.find(kind=['k-mers', 'k-mers and counts']):
-                meta = manifest[nm]
-                assert 'kind' in meta
-                if meta['kind'] == 'k-mers':
-                    (m, xs) = readKmers(z, nm)
-                    K = m['K']
-                    for x in xs:
-                        print render(K, x)
-                if meta['kind'] == 'k-mers and counts':
-                    (m, xs) = readKmers(z, nm)
-                    K = m['K']
-                    for (x, c) in xs:
-                        print '%s\t%d' % (render(K, x), c)
+    inp = opts['<input>']
+    with container(inp, 'r') as z:
+        K = z.meta['K']
+        if 'kmers' not in z.meta:
+            print >> sys.stderr, 'cannot dump "%s" as it contains no k-mers' % (inp,)
+            return
+        if 'counts' in z.meta:
+            xs = readKmersAndCounts(z)
+            for (x, c) in xs:
+                print '%s\t%d' % (render(K, x), c)
+        else:
+            xs = readKmers(z)
+            for x in xs:
+                print render(K, x)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
