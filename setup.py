76113c009c40868b0c543bab66d1164a5073813a
Thomas Conway <thomas.conway@petermac.org>
2017-03-05 08:19:53 +1100

    Version inc

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='zotmer',
         license='Apache2',
         keywords='bioinformatics genomics pathogenomics',
         packages=find_packages(),
-        install_requires=['docopt', 'pykmer>=0.2.1'],
+        install_requires=['docopt', 'pykmer>=0.3.0'],
         entry_points = {
             'console_scripts' : ['zot=zotmer.cli:main']
         },
