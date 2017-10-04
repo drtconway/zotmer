9eede6ff168bc16566f8ff50f0c35b56436ac429
Thomas Conway <drtomc@gmail.com>
2017-10-04 22:25:07 +1100

    Add a de Bruijn interpolator

--- zotmer/commands/hgvs-find.py
+++ zotmer/commands/hgvs-find.py
@@ -127,6 +127,53 @@ def computeBias(K, zs, verbose = False):
         S.add(v)
     return (S.mean(), S.var())
 
+
+class Interpolator(object):
+    def __init__(self, K, xs):
+        self.K = K
+        self.M = ((1 << (2*K)) - 1)
+        self.S = 2*(K-1)
+        self.xs = xs
+
+    def succ(self, x):
+        y0 = (x << 2) & self.M
+        for i in range(4):
+            y = y0 + i
+            if y in self.xs:
+                yield y
+
+    def pred(self, x):
+        y0 = x >> 2
+        for i in range(4):
+            y = y0 + (i << self.S)
+            if y in self.xs:
+                yield y
+
+    def path(self, xb, xe, n):
+        stk = [(xb, n+K, [])]
+        while len(stk) > 0:
+            (x0, n0, p0) = stk.pop()
+            if n0 == 0:
+                if x0 == xe:
+                    yield p0
+                continue
+            n1 = n0 - 1
+            for x1 in self.succ(x0):
+                p1 = p0 + [x0]
+                stk.append((x1, n1, p1))
+
+    def interpolate(self, xb, xe, n):
+        sMax = 0
+        pMax = None
+        for p in self.path(xb, xe, n):
+            s = 0
+            for x in p:
+                s += self.xs[x]
+            if s > sMax:
+                sMax = s
+                pMax = p
+        return pMax
+
 def hamming(K, xs, s):
     cs = [0 for j in range(K+1)]
     ws = [0 for j in range(K+1)]
