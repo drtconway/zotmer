7380bf0cf14fa0a05442fd14a21f4de64f17a21e
Tom Conway <tconway@au1.ibm.com>
2018-04-03 12:00:43 +1000

    Add a long del testcase.

---
+++ tests/hgvs-find/017-runme.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+wd=tmp/017
+mkdir -p ${wd}
+
+v="NC_000017.10:g.41197723_41197784delATCAGGTAGGTGTCCAGCTCCTGGCACTGGTAGAGTGCTACACTGTCCAACACCCACTCTCG"
+msg="long del ${v}"
+
+zot spikein -g ~/data/hg19 -b etc/BRCA1.bed -N 50000 -V 0.5 -S 17 -z ${wd}/reads ${v}
+
+zot hgvs-find -X -g ~/data/hg19 ${wd}/variant.idx ${v}
+
+zot hgvs-find -o ${wd}/output.txt ${wd}/variant.idx ${wd}/reads_*.fastq.gz
+
+cat > ${wd}/rules.py << EOF
+- if: n == '0'
+  then:
+    - res == 'wt/mut'
+    - 0.4 <= float(wtVaf) and float(wtVaf) <= 0.6
+    - 0.4 <= float(mutVaf) and float(mutVaf) <= 0.6
+EOF
+./bin/check ${wd}/rules.py ${wd}/output.txt > ${wd}/result.txt
+
+echo "$0" $(cat ${wd}/result.txt) ${msg}
+
+cleanup=yes
+if [ $(cat ${wd}/result.txt) == 'ACK' ] && [ ${cleanup} == 'yes' ]
+then
+    rm -rf ${wd}
+fi
